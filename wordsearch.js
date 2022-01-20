b7a516623c6422e27199837a7946f472934d67c5
Xinyu Chen <xinyuchen98@gmail.com>
2022-01-20 04:00:44 +0000

    Implemented word search vertically and horizontally

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
+const transpose = function(matrix) {
+    let outputMatrix = [];
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (!outputMatrix[j]) {
+          outputMatrix.push([matrix[i][j]]);
+        } else {
+          outputMatrix[j].push(matrix[i][j]);
+        }
+      }
+    }
+    return outputMatrix;
+  };
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    for (const l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+    return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
